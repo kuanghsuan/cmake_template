86f95ffc702a0d20264748e4a8986d6628aec4b0
Kuang Hsuan Lin <samcool199753@gmail.com>
2021-02-08 21:17:43 -0800

    git submodule

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include "adder.h"
+#include "GLFW/glfw3.h"
+
+int main () {
+    std::cout << "hey, sam" << std::endl;
+
+    std:: cout << add(72.1f, 0.0f) << std::endl;
+
+    GLFWwindow *window;
+
+    if (!glfwInit())
+    {
+        fprintf(stderr, "Failed to initialize GLFW\n");
+        exit(EXIT_FAILURE);
+    }
+    window = glfwCreateWindow(300, 300, "Gears", NULL, NULL);
+    if (!window)
+    {
+        fprintf(stderr, "Failed to open GLFW window\n");
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+
+    // Main loop
+    while (!glfwWindowShouldClose(window))
+    {
+
+        // Swap buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // Terminate GLFW
+    glfwTerminate();
+
+    return 0;
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+
+configure and build with cmakes using cnake-gui or using command line
+
+macOS command line
+    ./configure.sh
+    ./build.sh
+    ./run.sh
